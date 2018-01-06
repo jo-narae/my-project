01eb05b699b5e23618855324729663899fc42db2
jo-narae <nare6004@gmail.com>
2018-01-06 11:28:10 +0900

    #6 Chart add

--- src/components/Contents/Views/Dashboard.vue
+++ src/components/Contents/Views/Dashboard.vue
@@ -20,6 +20,38 @@
       </div>
     </div>
 
+    <!--Charts-->
+    <div class="row">
+      <div class="col-md-6 col-xs-12">
+        <chart-card :chart-data="preferencesChart.data"  chart-type="Pie">
+          <h4 class="title" slot="title">Email Statistics</h4>
+          <span slot="subTitle"> Last campaign performance</span>
+          <span slot="footer">
+            <i class="ti-timer"></i> Campaign set 2 days ago</span>
+          <div slot="legend">
+            <i class="fa fa-circle text-info"></i> Design
+            <i class="fa fa-circle text-danger"></i> Implement
+            <i class="fa fa-circle text-warning"></i> Verification
+            <i class="fa fa-circle text-warning"></i> Report
+            <i class="fa fa-circle text-warning"></i> Meeting
+          </div>
+        </chart-card>
+      </div>
+
+      <div class="col-md-6 col-xs-12">
+        <chart-card :chart-data="activityChart.data" :chart-options="activityChart.options">
+          <h4 class="title" slot="title">2015 Sales</h4>
+          <span slot="subTitle"> All products including Taxes</span>
+          <span slot="footer">
+            <i class="ti-check"></i> Data information certified</span>
+          <div slot="legend">
+            <i class="fa fa-circle text-info"></i> Tesla Model S
+            <i class="fa fa-circle text-warning"></i> BMW 5 Series
+          </div>
+        </chart-card>
+      </div>
+    </div>
+
   </div>
 </template>
 <script>
@@ -38,8 +70,16 @@
         statsCards: [
           {
             type: 'info',
+            icon: 'fa fa-inbox',
+            title: 'Total Event',
+            value: '23',
+            footerText: 'Updated now',
+            footerIcon: 'fa fa-refresh'
+          },
+          {
+            type: 'success',
             icon: 'fa fa-pie-chart',
-            title: 'Progress Rate',
+            title: 'Progress Date',
             value: '23',
             sign: '%',
             footerText: 'Updated now',
@@ -48,18 +88,10 @@
           {
             type: 'danger',
             icon: 'fa fa-history',
-            title: 'Delay Count',
+            title: 'Total Delay',
             value: '30',
             footerText: 'Updated now',
             footerIcon: 'fa fa-refresh'
-          },
-          {
-            type: 'success',
-            icon: 'fa fa-calendar-check-o',
-            title: 'Progress Date',
-            value: '30',
-            footerText: 'View calendar',
-            footerIcon: 'fa fa-calendar'
           },
           {
             type: 'warning',
@@ -69,7 +101,30 @@
             footerText: 'View members',
             footerIcon: 'fa fa-search'
           }
-        ]
+        ],
+        preferencesChart: {
+          data: {
+            labels: ['30%', '30%', '20%', '10%', '10%'],
+            series: [30, 30, 20, 10, 10]
+          },
+          options: {}
+        },
+        activityChart: {
+          data: {
+            labels: ['Jan', 'Feb', 'Mar', 'Apr', 'Mai', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+            series: [
+              [542, 543, 520, 680, 653, 753, 326, 434, 568, 610, 756, 895],
+              [230, 293, 380, 480, 503, 553, 600, 664, 698, 710, 736, 795]
+            ]
+          },
+          options: {
+            seriesBarDistance: 10,
+            axisX: {
+              showGrid: false
+            },
+            height: '245px'
+          }
+        }
       }
     }
   }
