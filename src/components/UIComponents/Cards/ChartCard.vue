fb946ba45ceaf2b63ca7b52df35d8f097e2ec043
jo-narae <nare6004@gmail.com>
2018-01-08 01:32:11 +0900

    #6 total issue chart

--- src/components/UIComponents/Cards/ChartCard.vue
+++ src/components/UIComponents/Cards/ChartCard.vue
@@ -3,7 +3,7 @@
     <div class="content">
       <div class="row">
         <div class="col-xs-12 col-md-12">
-          <line-chart :data="dataSet" />
+          <p class="title" slot="title" style="text-align: center; font-size: 14pt; margin-bottom: 10px;">Total Issue Count of This Week</p>
           <bar-chart :data="dataSet" :options="defaultOption" />
         </div>
       </div>
@@ -21,21 +21,53 @@
     data () {
       return {
         dataSet: {
-          labels: ['January', 'February'],
+          labels: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
           datasets: [
             {
-              label: 'GitHub Commits',
-              backgroundColor: '#f87979cc',
-              data: [40, 20]
+              label: 'Stay',
+              backgroundColor: '#427C89',
+              borderColor: '#fff',
+              hoverBackgroundColor: '#427C89',
+              data: [10, 20, 30, 40, 50, 60, 70]
             },
             {
-              label: 'NaRae Commits',
-              backgroundColor: '#8db1f8cc',
-              data: [30, 50]
+              label: 'Working',
+              backgroundColor: '#41B883',
+              borderColor: '#fff',
+              hoverBackgroundColor: '#41B883',
+              data: [5, 10, 15, 20, 25, 30, 40]
+            },
+            {
+              label: 'Complete',
+              backgroundColor: '#3091B2',
+              borderColor: '#fff',
+              hoverBackgroundColor: '#3091B2',
+              data: [10, 20, 30, 40, 50, 60, 70]
+            },
+            {
+              label: 'Reject',
+              backgroundColor: '#F3BB45',
+              borderColor: '#fff',
+              hoverBackgroundColor: '#F3BB45',
+              data: [2, 4, 6, 8, 10, 12, 14]
+            },
+            {
+              label: 'Close',
+              backgroundColor: '#B33C12',
+              borderColor: '#fff',
+              hoverBackgroundColor: '#B33C12',
+              data: [100, 110, 130, 140, 160, 180, 200]
             }
           ]
         },
         defaultOption: {
+          maintainAspectRatio: false,
+          padding: {
+            left: 10,
+            right: 10,
+            top: 50,
+            bottom: 100
+          },
           scales: {
             xAxes: [{
               stacked: true
@@ -49,6 +81,5 @@
     }
   }
 </script>
-<style>
-
+<style scoped>
 </style>
